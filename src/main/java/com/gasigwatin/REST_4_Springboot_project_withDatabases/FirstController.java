244db6b2d7d3b22df3ff8e795653115f25f7dc59
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-05-15 13:53:00 +0200

    mayday

---
+++ src/main/java/com/gasigwatin/REST_4_Springboot_project_withDatabases/FirstController.java
@@ -0,0 +1,11 @@
+package com.gasigwatin.REST_4_Springboot_project_withDatabases;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    public String getMessage() {
+        return "";
+    }
+}
